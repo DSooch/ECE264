9c542dc2c3ad3f2645a19d0d906046f7dea17369
Dan Suciu <suciu@ecegrid-thin1.ecn.purdue.edu>
2014-10-07 16:16:30 -0400

    Final Solution to PA05

--- Fall2014/PA05/answer05.c
+++ Fall2014/PA05/answer05.c
@@ -1,29 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MAXLEN 100;
-
 void printPartition(int * arr, int length)
 {
-	int index;
+	int count;
 
-	for (index = 0; index < (length - 1); index++)
+	printf("= ");
+
+	for (count = 0; count < (length - 1); count++)
 	{
-		if (index == 0)
-		{
-			printf("= ");
-		} else
-		{
-			printf("%d + ", arr[index]);
-		}
+		printf("%d + ", arr[count]);
 	}
 
-	printf("%d\n", arr[index]);
+	printf("%d\n", arr[count]);
 
 	return;
 }
 
-//WAS HELPED IN WRITING THIS FUNCTION BY A TA IN THE EE 206 HELP ROOM
+//WAS HELPED IN WRITING THIS FUNCTION BY A TA IN THE EE-206 HELP ROOM
 
 void partitionHelper(int * buffer, int index, int value, int (* compFunc) (int value, int * arr, int index))
 {
@@ -52,12 +46,34 @@ int all(int value, int * arr, int index)
 
 int increasing(int value, int * arr, int index)
 {
-	return (0);
+	if (index == 0)
+	{
+		return (1);
+	}
+
+	if (value > arr[index - 1])
+	{
+		return (1);
+	} else 
+	{
+		return (0);
+	}
 }
 
 int decreasing(int value, int * arr, int index)
 {
-	return (0);
+	if (index == 0)
+	{
+		return (1);
+	}
+
+	if (value < arr[index - 1])
+	{
+		return (1);
+	} else 
+	{
+		return (0);
+	}
 }
 
 int odd(int value, int * arr, int index)
@@ -84,61 +100,90 @@ int even(int value, int * arr, int index)
 
 int oddEven(int value, int * arr, int index)
 {
-	return (0);
+	if (index == 0)
+	{
+		return (1);
+	}
+
+	if ((value % 2 == 0) && (arr[index - 1] % 2 == 1))
+	{
+		return (1);
+	} else if ((value % 2 == 1) && (arr[index - 1] % 2 == 0))
+	{
+		return (1);
+	} else 
+	{
+		return (0);
+	}
 }
 
 int prime(int value, int * arr, int index)
 {
-	return (0);
+	int count;
+
+	if (value <= 1)
+	{
+		return (0);
+	}
+
+	for (count = 2; count < value; count++)
+	{
+		if (value % count == 0)
+		{
+			return (0);
+		}
+	}
+
+	return (1);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 void partitionAll(int value)
 {
-	int arr[MAXLEN];
+	int arr[100];
 	partitionHelper(arr, 0, value, all);
 	return;
 }
 
 void partitionIncreasing(int value)
 {
-	int arr[MAXLEN];
+	int arr[100];
 	partitionHelper(arr, 0, value, increasing);
 	return;
 }
 
 void partitionDecreasing(int value)
 {
-	int arr[MAXLEN];
+	int arr[100];
 	partitionHelper(arr, 0, value, decreasing);
 	return;
 }
 
 void partitionOdd(int value)
 {
-	int arr[MAXLEN];
+	int arr[100];
 	partitionHelper(arr, 0, value, odd);
 	return;
 }
 
 void partitionEven(int value)
 {
-	int arr[MAXLEN];
+	int arr[100];
 	partitionHelper(arr, 0, value, even);
 	return;
 }
 
 void partitionOddAndEven(int value)
 {
-	int arr[MAXLEN];
+	int arr[100];
 	partitionHelper(arr, 0, value, oddEven);
 	return;
 }
 
 void partitionPrime(int value)
 {
-	int arr[MAXLEN];
+	int arr[100];
 	partitionHelper(arr, 0, value, prime);
 	return;
 }
