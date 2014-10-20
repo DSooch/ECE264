0024413070f8e173991c9a5f593da88ffd5484d4
Dan Suciu <suciu@ecegrid-thin1.ecn.purdue.edu>
2014-10-20 18:14:29 -0400

    Creation of PA07 Assignment

--- Fall2014/PA07/answer07.h
+++ Fall2014/PA07/answer07.h
@@ -1,87 +1,78 @@
 /**
- * Do not modify this file
+ * Do not modify this file.
  */
 #ifndef PA07_H
 #define PA07_H
 
-/**
- * A ListNode structure for a linked list of strings.
- */
-typedef struct ListNode_st
-{
-    char * str;
-    struct ListNode_st * next;
-} List;
+#include <stdint.h>
+
+#define ECE264_IMAGE_MAGIC_NUMBER 0x21343632
+
+// Tell gcc not to pad structs with extra bytes.
+// Padding is sometimes included for performance reasons.
+#pragma pack(1)
+
+typedef struct ImageHeader_st {
+    uint32_t magic_number; // Should be ECE264_IMAGE_MAGIC_NUMBER
+    uint32_t width;        // [width x height], cannot be zero
+    uint32_t height;
+    uint32_t comment_len;  // A comment embedded in the file
+} ImageHeader;
+
+typedef struct Image_st {
+    int width;
+    int height;
+    char * comment;
+    uint8_t * data;
+} Image;
 
 /**
- * Create a new list-node with the passed string. 
- * str should be copied (with strdup).
+ * Loads a bmp (windows bitmap) image, returning an Image structure.
+ * Will return NULL if there is any error.
+ *
+ * Note: This function is written for you and appears in image-bmp.c
  */
-List * List_createNode(const char * str);
+Image * Image_loadbmp(const char * filename);
 
 /**
- * Free all memory associated with the linked list, including memory for
- * contained strings. Must safely handle NULL lists.
+ * Saves an Image structure to a file. Returns TRUE if success, or
+ * FALSE if there is any error.
+ *
+ * Note: This function is written for you and appears in image-bmp.c
  */
-void List_destroy(List * list);
+int Image_savebmp(const char * filename, Image * image);
 
 /**
- * Length of a linked list. 
- * The length of "NULL" is 0.
+ * Loads an ECE264 image file, returning an Image structure.
+ * Will return NULL if there is any error.
+ *
+ * Hint: Please see the README for extensive hints
  */
-int List_length(List * list);
+Image * Image_load(const char * filename);
 
 /**
- * Merge two sorted lists to produce a final sorted list.
- * 
- * 'lhs' and 'rhs' are two sorted linked-list. Read 'left-hand-side' and 'right-
- * hand-side'. Note that either (or both) could be NULL, signifying the empty
- * list.
- * 'compar' is a function that is used to compare two nodes. This parameter is
- * similar to the parameter in qsort(...), except that it only compares two
- * strings. You can pass 'strcmp' to this function if you want to sort lists in
- * ascending order.
+ * Save an image to the passed filename, in ECE264 format.
+ * Return TRUE if this succeeds, or FALSE if there is any error.
  *
- * When implementing this function, do not call List_createNode(...) or
- * malloc(...) Instead, think about how to build a new list by:
- * (1) Create a new (empty -- i.e., NULL) list where we build the result. You 
- *     will need to track the first and last node of this list.
- * (2) In a while-loop:
- * (2.a) Use compar to identify whether the front node of lhs or rhs is smaller.
- *       Remember that lhs or rhs could be NULL. 
- * (2.b) Move the front node of 'lhs/rhs' to the end of the result list. 
- *
- * You will need to consider what happens when 'lhs' or 'rhs' becomes NULL.
- *
- * Well-written code should be 20-30 lines long, including comments and spacing.
- * If your code is longer than this, then you may save time by rethinking your
- * approach.
+ * Hint: Please see the README for extensive hints
  */
-List * List_merge(List * lhs, 
-		  List * rhs, 
-		  int (*compar)(const char *, const char*));
+int Image_save(const char * filename, Image * image);
 
-/** 
- * Sorts the list's elements using the merge-sort algorithm.
- * Merge-sort is a recursive algorithm. See the README for hints.
- * 
- * The brief instructions are as follows:
+/**
+ * Free memory for an image structure
  *
- * (1) Base case: 
- * Lists of length 0 or 1 are already (defacto) sorted. In this case, return
- * 'list'.
- *
- * (2) Recursive case: 
- * (2.a) Split the linked-list into two approx. equal sized lists.
- * (2.b) Call List_sort(...) on each of these smaller lists.
- * (2.c) Call List_merge(...) to merge the now sorted smaller lists into a 
- *       single larger sorted list, which you return.
- *
- * Well-written code should be 20-30 lines long, including comments and spacing.
- * If your code is longer than this, then you may save time by rethinking your
- * approach.
+ * Image_load(...) (above) allocates memory for an image structure. 
+ * This function must clean up any allocated resources. If image is 
+ * NULL, then it does nothing. (There should be no error message.) If 
+ * you do not write this function correctly, then valgrind will 
+ * report an error. 
  */
-List * List_sort(List * list, int (*compar)(const char *, const char*));
+void Image_free(Image * image);
+
+/**
+ * Performs linear normalization, see README
+ */
+void linearNormalization(int width, int height, uint8_t * intensity);
 
 #endif
 
