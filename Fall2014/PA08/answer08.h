af83eeb9e42ecb7a81d26d454328518d7518706b
Dan Suciu <suciu@ecegrid-thin1.ecn.purdue.edu>
2014-10-28 14:06:54 -0400

    Uploading PA08

--- Fall2014/PA08/answer08.h
+++ Fall2014/PA08/answer08.h
@@ -1,209 +1,87 @@
 /**
  * Do not modify this file
  */
-
-#ifndef ANSWER08_H
-#define ANSWER08_H
+#ifndef PA08_H
+#define PA08_H
 
 /**
- * A Binary Search Tree Node structure to store a sparse array element.
+ * A ListNode structure for a linked list of strings.
  */
-typedef struct snode
+typedef struct ListNode_st
 {
-    int index;
-    int value;
-    struct snode * left;
-    struct snode * right;
-} SparseNode;
+    char * str;
+    struct ListNode_st * next;
+} List;
 
 /**
- * Create a single instance of a sparse array tree node with a specific
- * index and value. This sparse array will be implemented by a binary search
- * tree.
- *
- * Arguments:
- * index         the index to be stored in the node
- * value         the value to be stored in the node
- *
- * returns:
- * SparseNode *  the pointer points to the node just constructed
- *
- * This is a constructor function that allocates memory for a sparse array tree
- * node, sets the given index and value, and initialize the subtree pointers to
- * NULL.
+ * Create a new list-node with the passed string. 
+ * str should be copied (with strdup).
  */
-SparseNode * SparseNode_create(int index, int value);
-
-/* Add a particular value into a sparse array tree at a particular index.
- *
- * Arguments:
- * array         the root node of the sparse array tree
- * index         the index to be stored in the node
- * value         the value to be stored in the node
- *
- * returns:
- * SparseNode *  the pointer points to the root node of the modified sparse
- *               array tree
- *
- * The sparse array tree uses the index as a key in a binary search tree.
- * If the index does not exist, create it.
- * If the index exists, REPLACE the value with the new one.
- * Use the index to determine whether to go left or right in the tree (smaller
- * index values than the current one go left, larger ones go right).
- * (For this assignment, you should not insert nodes with a value of 0)
- */
-SparseNode * SparseArray_insert(SparseNode * array, int index, int value);
+List * List_createNode(const char * str);
 
 /**
- * Build a sparse array tree from given array of indices and array of values,
- * each having the same length.
- *
- * Arguments:
- * indices      a pointer pointing to the start position of the index array
- * values       a pointer pointing to the start position of the value array
- * length       the size of each of the above input arrays
- *
- * returns:
- * SparseNode *     the pointer points to the root node of sparse array
- *                  tree just constructed
- *
- * It returns a sparse array tree.  You need to insert tree nodes in order,
- * starting from index zero of the array.
- *
- * i.e. The first sparse array node contains indices[0] and values[0], second node
- * contains indices[1] and values[1], etc. Basically, elements of the index array
- *  and value array that have the same array index form a sparse array pair 
- * and should go into the same sparse array node.
+ * Free all memory associated with the linked list, including memory for
+ * contained strings. Must safely handle NULL lists.
  */
-SparseNode * SparseArray_build(int * indices, int * values, int length);
+void List_destroy(List * list);
 
 /**
- * Destroy an entire sparse array node and its subtrees.
- *
- * Arguments:
- * array          the root node of a sparse array tree
- *
- * returns: void
- *
- * Traversing the binary tree in postorder, use the SparseArray_destroy()
- * function to destroy each node individually.
+ * Length of a linked list. 
+ * The length of "NULL" is 0.
  */
-void SparseArray_destroy(SparseNode * array);
+int List_length(List * list);
 
 /**
- * Retrieve the smallest index in the sparse array tree.
+ * Merge two sorted lists to produce a final sorted list.
+ * 
+ * 'lhs' and 'rhs' are two sorted linked-list. Read 'left-hand-side' and 'right-
+ * hand-side'. Note that either (or both) could be NULL, signifying the empty
+ * list.
+ * 'compar' is a function that is used to compare two nodes. This parameter is
+ * similar to the parameter in qsort(...), except that it only compares two
+ * strings. You can pass 'strcmp' to this function if you want to sort lists in
+ * ascending order.
  *
- * Arguments:
- * array        the root node of a sparse array tree, is never NULL
+ * When implementing this function, do not call List_createNode(...) or
+ * malloc(...) Instead, think about how to build a new list by:
+ * (1) Create a new (empty -- i.e., NULL) list where we build the result. You 
+ *     will need to track the first and last node of this list.
+ * (2) In a while-loop:
+ * (2.a) Use compar to identify whether the front node of lhs or rhs is smaller.
+ *       Remember that lhs or rhs could be NULL. 
+ * (2.b) Move the front node of 'lhs/rhs' to the end of the result list. 
  *
- * returns:
- * int          the smallest index in the sparse array tree
+ * You will need to consider what happens when 'lhs' or 'rhs' becomes NULL.
  *
- * If array is NULL then it does not make sense to get the max value,
- * so you are free to do whatever you think is appropriate. 
- *
- * (Hint: consider the property of binary search tree)
+ * Well-written code should be 20-30 lines long, including comments and spacing.
+ * If your code is longer than this, then you may save time by rethinking your
+ * approach.
  */
-int SparseArray_getMin(SparseNode * array);
+List * List_merge(List * lhs, 
+		  List * rhs, 
+		  int (*compar)(const char *, const char*));
 
-/**
- * Retrieve the largest index in the sparse array tree.
+/** 
+ * Sorts the list's elements using the merge-sort algorithm.
+ * Merge-sort is a recursive algorithm. See the README for hints.
+ * 
+ * The brief instructions are as follows:
  *
- * Arguments:
- * array        the root node of a sparse array tree, is never NULL
+ * (1) Base case: 
+ * Lists of length 0 or 1 are already (defacto) sorted. In this case, return
+ * 'list'.
  *
- * returns:
- * int          the largest index in the sparse array tree
+ * (2) Recursive case: 
+ * (2.a) Split the linked-list into two approx. equal sized lists.
+ * (2.b) Call List_sort(...) on each of these smaller lists.
+ * (2.c) Call List_merge(...) to merge the now sorted smaller lists into a 
+ *       single larger sorted list, which you return.
  *
- * If array is NULL then it does not make sense to get the max value,
- * so you are free to do whatever you think is appropriate. 
- *
- * (Hint: consider the property of binary search tree)
+ * Well-written code should be 20-30 lines long, including comments and spacing.
+ * If your code is longer than this, then you may save time by rethinking your
+ * approach.
  */
-int SparseArray_getMax(SparseNode * array);
-
-/**
- * Retrieve the node associated with a specific index in a sparse array tree.
- *
- * Arguments:
- * array        the root node of a sparse array tree
- * index        the index of the node you want to search for
- *
- * returns:
- * SparseNode *     the node with the index that you searched from the tree.
- *                  If no node is found, NULL should be returned.
- *
- * (Hint: consider the property of binary search tree)
- */
-SparseNode * SparseArray_getNode(SparseNode * array, int index);
-
-/**
- * Remove a value associated with a particular index from the sparse array. It
- * returns the new sparse array tree ( binary tree root ).
- *
- * Arguments:
- * array        the root node of a sparse array tree
- * index        the index of the node you want to remove
- *
- * returns:
- * SparseNode *     the root node of the sparse array tree that you just
- *                  modified
- *
- * HINT : First, you need to go to that node. Then, you will need to isolate
- * several different cases here :
- * - If array is empty ( NULL ), return NULL.
- * - Go left or right if the current node index is different.
- * - If both subtrees are empty, you can just remove the node.
- * - If one subtree is empty, you can just remove the current node and replace it
- *   with the non - empty child.
- * - If both children exist, you must find the immediate successor of the
- *   current node ( leftmost of right branch ), swap its values with the
- *   current node ( BOTH index and value ), and then delete the index in the
- *   right subtree.
- */
-SparseNode * SparseArray_remove(SparseNode * array, int index);
-
-/**
- * The function makes a copy of the input sparse array tree and returns this
- * new copy.
- *
- * Arguments:
- * array            the root node of a sparse array tree
- *
- * returns:
- * SparseNode *     the root node of the new sparse array tree that you
- *                  just copied from the input sparse array tree.
- *
- * To copy the array you will need to traverse the tree. See README Hints on
- * iterate.
- */
-SparseNode * SparseArray_copy(SparseNode * array);
-
-/**
- * Merge array_1 and array_2, and return the result array.  This function WILL
- * NOT CHANGE the contents in array_1 and array_2.
- *
- * Arguments:
- * array_1          the root node of the first sparse array tree
- * array_2          the root node of the second sparse array tree
- *
- * returns:
- * SparseNode *     the root node of the new sparse array tree that you
- *                  just merged from the two input sparse array tree
- *
- * When merging two sparse array tree:
- * 1. The contents in array_1 and array_2 should not be changed. You should
- *    make a copy of array_1, and do merging in this copy.
- * 2. array_2 will be merged to array_1. This means you need to read nodes in
- *    array_2 and insert them into array_1.
- * 3. You need to traverse the array_2 tree. (See README Hints on iterate.)
- * 4. Values of two nodes need to be added only when the indices are the same.
- * 5. A node with value of 0 should be removed.
- * 6. if array_2 has nodes with index different than any nodes in array_1, you
- *    should insert those nodes into array_1.
- *
- * Hint: you may write new functions
- */
-SparseNode * SparseArray_merge(SparseNode * array_1, SparseNode * array_2);
+List * List_sort(List * list, int (*compar)(const char *, const char*));
 
 #endif
+
--- Fall2014/PA08/testcases/test_support_functions.c
+++ Fall2014/PA08/testcases/test_support_functions.c
@@ -2,153 +2,84 @@
 #define TRUE 1
 #define FALSE 0
 
-static int SparseNode_size(SparseNode * node)
+static int strcmp_forward(const char * str_a, const char * str_b)
 {
-    if(node == NULL)
-	return 0;
-    return 1 + SparseNode_size(node->left) + SparseNode_size(node->right);
+    return strcmp(str_a, str_b);
 }
 
-static void SparseNode_toArrayHelper(SparseNode * node, int ** indices, int ** values)
+static int strcmp_reverse(const char * str_a, const char * str_b) 
 {
-    if(node == NULL) 
-	return;
-    SparseNode_toArrayHelper(node->left, indices, values);
-    **indices = node->index;
-    **values = node->value;
-    (*indices)++;
-    (*values)++;
-    SparseNode_toArrayHelper(node->right, indices, values);
+    return -strcmp(str_a, str_b);
 }
 
-static int SparseNode_compare(SparseNode * root1, SparseNode * root2)
+static int strcmp_length(const char * str_a, const char * str_b)
 {
-    int len1 = SparseNode_size(root1);
-    int * indices1 = malloc(len1 * sizeof(int));
-    int * values1 = malloc(len1 * sizeof(int));
+    return strlen(str_a) - strlen(str_b);
+}
 
-    int len2 = SparseNode_size(root2);
-    int * indices2 = malloc(len2 * sizeof(int));
-    int * values2 = malloc(len2 * sizeof(int));
-    
-    {
-	int * indices_itr = indices1;
-	int * values_itr = values1;
-	SparseNode_toArrayHelper(root1, &indices_itr, &values_itr);
+static int strcmp_int(const char * str_a, const char * str_b)
+{
+    return atoi(str_a) - atoi(str_b);
+}
+
+static void List_print(List * list)
+{
+    List * tail = list;
+    while(tail != NULL) {
+    	printf("\"%s\" ==> ", tail->str);
+    	tail = tail->next;
     }
+    printf("NULL");
+}
 
-    {
-	int * indices_itr = indices2;
-	int * values_itr = values2;
-	SparseNode_toArrayHelper(root2, &indices_itr, &values_itr);
-    }
+static void print_compar(int (*compar)(const char *, const char *)) 
+{
+    if(compar == strcmp_forward) printf("return strcmp(a, b)");
+    else if(compar == strcmp_reverse) printf("return -strcmp(a, b)");
+    else if(compar == strcmp_length) printf("return strlen(a) - strlen(b)");
+    else if(compar == strcmp_int) printf("return atoi(a) - atoi(b)");
+    else printf("[UNKNOWN]");
+}
 
-    int ret = TRUE;
-
-    if(len1 != len2)
-	ret = FALSE;
-    
-    int ind;
-    for(ind = 0; ind < len1 && ret; ++ind) {
-	if(indices1[ind] != indices2[ind]) ret = FALSE;
-	if(values1[ind] != values2[ind]) ret = FALSE;
-    }
-
-    free(indices1); 
-    free(indices2);
-    free(values1);
-    free(values2);
-
+static List * List_create(const char * * strArr, int len)
+{
+    if(len == 0)
+	return NULL;
+    List * ret = List_createNode_sol(strArr[0]);
+    ret->next = List_create(++strArr, len - 1);
     return ret;
 }
 
-static int cmp_array(SparseNode * ans, SparseNode * sol) {
-    return SparseNode_compare(ans, sol);
-    /* if(ans == NULL && sol == NULL) { */
-    /*     return TRUE; */
-    /* } */
-    /* else if(ans != NULL && sol != NULL) { */
-    /*     int sameleft = FALSE; */
-    /*     int sameright = FALSE; */
-    /*     sameleft = cmp_array(ans->left, sol->left); */
-    /*     sameright = cmp_array(ans->right, sol->right); */
-    /*     if(sameleft &&  */
-    /*             sameright &&  */
-    /*             ans->index == sol->index &&  */
-    /*             ans->value == sol->value) { */
-    /*         return TRUE; */
-    /*     } */
-    /*     else { */
-    /*         return FALSE; */
-    /*     } */
-    /* } */
-    /* else { */
-    /*     return FALSE; */
-    /* } */
-}
-
-static int digits(int x) {
-    int num = 0;
-    if(x < 0) {
-        x = -x;
-        num++;
-    }
-    while(x>=1){
-        x = x/10;
-        num++;
-    }
-    return num;
-}
-
-static void print_array(SparseNode * array, int len) {
-    if(array == NULL) {
-        return;
-    }
-    printf("(%d,%d)",array->index, array->value);
-    len += (digits(array->index) + digits(array->value) + 3);
-    if(array->right != NULL){
-        printf("->");
-        print_array(array->right, len+2);
-    }
-    else {
-        printf("\n");
-    }
-    if(array->left != NULL) {
-        int i;
-        for(i = 0; i < len; i++) {
-            printf(" ");
-        }
-        printf("->");
-        print_array(array->left, len+2);
-    }
-}
-
-static SparseNode * make_array(int array_number)
+static List * List_clone(List * list)
 {
-    int a0[2][4] = { {1,3,0,8}, {5,3,6,4} };
-    int a1[2][9] = { {4,2,0,40000,6,30,1,5,4}, {30,22,100,3,5,-7,-30,0,20} };
-    int a2[2][7] = { {50,25,75,12,30,60,80}, {4,7,-3,9,80,-7,-30} };
-    int a3[2][13] = {
-        {30,40,50,60,70,80,90,28,25,22,9,6,0},
-        {30,2,3,7,30,80,-70,-300,10,200,22,10,65}
-    };
-    int a4[2][10] = {
-        {20,30,25,8,19,18,17,15,16,6},
-        {59,3,100,-3,5,2,7,-3,4,20}
-    };
-    int a5[2][7] = { {1,10,25,8,4,7,11}, {5,3,3,4,-30,20,-4} };
-    int a6[2][6] = { {100,36,0,12,9,120}, {50,33,3,100,-6,4} };
-    int a7[2][0] = { {}, {} };
+    if(list == NULL) 
+	return NULL;
+    List * ret = List_createNode_sol(list->str);
+    ret->next = List_clone(list->next);
+    return ret;
+}
 
-    switch(array_number) {
-    case 0: return SparseArray_build_sol(a0[0], a0[1], sizeof(a0[0]) / sizeof(const int));
-    case 1: return SparseArray_build_sol(a1[0], a1[1], sizeof(a1[0]) / sizeof(const int));
-    case 2: return SparseArray_build_sol(a2[0], a2[1], sizeof(a2[0]) / sizeof(const int));
-    case 3: return SparseArray_build_sol(a3[0], a3[1], sizeof(a3[0]) / sizeof(const int));
-    case 4: return SparseArray_build_sol(a4[0], a4[1], sizeof(a4[0]) / sizeof(const int));
-    case 5: return SparseArray_build_sol(a5[0], a5[1], sizeof(a5[0]) / sizeof(const int));
-    case 6: return SparseArray_build_sol(a6[0], a6[1], sizeof(a6[0]) / sizeof(const int));
-    case 7: return SparseArray_build_sol(a7[0], a7[1], sizeof(a7[0]) / sizeof(const int));
+static List * make_list(int list_number)
+{
+    const char * l0[] = { "b", "d", "f" };
+    const char * l1[] = { "a", "c", "e", "g", "h", "i", "j" };
+    const char * l2[] = { "2", "3", "5", "7" };
+    const char * l3[] = { "11", "13", "17", "19", "23" };
+    const char * l4[] = { "2010" };
+    const char * l5[] = { "Hello", "Doctor", "Name", "Continue", "Yesterday", 
+			  "Tomorrow" };
+    const char * l6[] = { "sea", "a", "be" };
+    const char * l7[] = { "deee" };
+
+    switch(list_number) {
+    case 0: return List_create(l0, sizeof(l0) / sizeof(const char *));
+    case 1: return List_create(l1, sizeof(l1) / sizeof(const char *));
+    case 2: return List_create(l2, sizeof(l2) / sizeof(const char *));
+    case 3: return List_create(l3, sizeof(l3) / sizeof(const char *));
+    case 4: return List_create(l4, sizeof(l4) / sizeof(const char *));
+    case 5: return List_create(l5, sizeof(l5) / sizeof(const char *));
+    case 6: return List_create(l6, sizeof(l6) / sizeof(const char *));
+    case 7: return List_create(l7, sizeof(l7) / sizeof(const char *));
     }
     return NULL;
 }
