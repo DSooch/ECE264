992bbf6bda980fe22579d50d3d0b890491421d90
Dan Suciu <suciu@ecegrid-thin1.ecn.purdue.edu>
2014-12-03 19:12:41 -0500

    Final Solution to PA11

--- Fall2014/PA11/answer11.c
+++ Fall2014/PA11/answer11.c
@@ -4,6 +4,8 @@
 
 #define TRUE 1
 #define FALSE 0
+#define ONE 49
+#define ZERO 48
 
 // -------------------------------------------------------------------- HuffNode
 
@@ -91,7 +93,10 @@ void Stack_destroy(Stack * stack)
 
 int Stack_isEmpty(Stack * stack)
 {
-	if (stack -> head == NULL || stack == NULL)
+	if (stack == NULL)
+	{
+		return TRUE;
+	} else if (stack -> head == NULL)
 	{
 		return TRUE;
 	} else 
@@ -100,27 +105,11 @@ int Stack_isEmpty(Stack * stack)
 	}
 }
 
-/**
- * Pop the front (top) 'value' (i.e. Huffman tree) from the stack.
- *
- * More precisely, this function must do three things:
- * (1) Save the value (i.e. Huffman tree) of the head node of the stack's list
- * (2) Remove the head node of the stack's list, freeing it.
- * (3) Return the value (i.e. Huffman tree) saved in (1).
- */
 HuffNode * Stack_popFront(Stack * stack)
 {
-	HuffNode * val = HuffNode_create(1);
+	HuffNode * val = stack -> head -> tree;
 
-	val = stack -> head -> tree;
-
-	StackNode * temp = malloc(sizeof(StackNode));
-
-	temp = stack -> head -> next;
-
-	HuffNode_destroy(stack -> head -> tree);
-	stack -> head -> tree = NULL;
-	stack -> head -> next = NULL;
+	StackNode * temp = stack -> head -> next;
 
 	free(stack -> head);
 
@@ -129,18 +118,9 @@ HuffNode * Stack_popFront(Stack * stack)
 	return (val);
 }
 
-/*
- * Push a 'value' (i.e. Huffman tree) onto the front (top) of the stack.
- *
- * More precisely, this function must do two things:
- * (1) Create a new StackNode with 'tree' for its tree.
- * (2) Push that new StackNode onto the front of the stack's list.
- */
 void Stack_pushFront(Stack * stack, HuffNode * tree)
 {
 	StackNode * newNode = malloc(sizeof(StackNode));
-
-	newNode -> tree = HuffNode_create(1);
 
 	newNode -> tree = tree;
 
@@ -151,32 +131,14 @@ void Stack_pushFront(Stack * stack, HuffNode * tree)
 	return;
 }
 
-// -------------------------------------------------------- Pop Pop combine Push
-
-/**
- * This function helps simplify building a Huffman Coding Tree from the header
- * information. It takes a stack as input. As a precondition, you can assume 
- * that the stack has at least two nodes. This function pops the front (top) 
- * two nodes, combines them into a single node, and pushes the new node back 
- * onto the stack. See Huffman_Coding.pdf to understand conceptually how this
- * should be done.
- */
 void Stack_popPopCombinePush(Stack * stack)
 {
 	HuffNode * first = Stack_popFront(stack);
 	HuffNode * second = Stack_popFront(stack);
 	HuffNode * final = HuffNode_create(1);
 
-	if (first -> value >= second -> value)
-	{
-		final -> right = first;
-		final -> left = second;
-
-	} else 
-	{
-		final -> left = first;
-		final -> right = second;
-	}
+	final -> left = second;
+	final -> right = first;
 
 	Stack_pushFront(stack, final);
 	
@@ -190,7 +152,39 @@ void Stack_popPopCombinePush(Stack * stack)
  */
 HuffNode * HuffTree_readTextHeader(FILE * fp)
 {
-	return;
+	int ch;
+	int done = 0;
+
+	Stack * stack = Stack_create();
+
+	while (!done)
+	{
+		if (fgetc(fp) == '1')
+		{
+			ch = fgetc(fp);
+
+			HuffNode * new = HuffNode_create(ch);
+
+			Stack_pushFront(stack, new);
+		} else 
+		{
+			if (stack -> head -> next == NULL)
+			{
+				done = 1;
+			} else
+			{
+				Stack_popPopCombinePush(stack);
+			}
+		}
+	}
+
+	HuffNode * final = stack -> head -> tree;
+
+	free(stack -> head);
+
+	free(stack);
+
+	return (final);
 }
 
 /**
@@ -198,7 +192,69 @@ HuffNode * HuffTree_readTextHeader(FILE * fp)
  * You will need to (conceptually) read a file a bit at a time. See the README
  * for hints on how to do this.
  */
+int readBit(FILE * fp, unsigned char * bit, unsigned char * whichbit, unsigned char * curbyte)
+{
+	int ret = 1;
+	if ((* whichbit) == 0)
+	{
+		ret = fread(curbyte, sizeof(unsigned char), 1, fp);
+	}
+	if (ret != 1)
+	{
+		return (-1);
+	}
+
+	unsigned char temp = (* curbyte) >> (7 - (* whichbit));
+	temp = temp & 0X01;
+	* whichbit = ((* whichbit) + 1) % 8;
+	* bit = temp;
+	return 1;
+}
+
 HuffNode * HuffTree_readBinaryHeader(FILE * fp)
 {
-	return;
+	int done = 0;
+	unsigned char whichbit = 0;
+	unsigned char curbyte = 0;
+	unsigned char onebit = 0;
+	Stack * stack = Stack_create();
+
+	while (done == 0)
+	{
+		readBit(fp, & onebit, & whichbit, & curbyte);
+		if (onebit == 1)
+		{
+			int bitcount;
+			unsigned char value = 0;
+			for (bitcount = 0; bitcount < 8; bitcount++)
+			{
+				value <<= 1;
+				readBit(fp, & onebit, & whichbit, & curbyte);
+				value |= onebit;
+			}
+			HuffNode * hn = HuffNode_create(value);
+			Stack_pushFront(stack, hn);
+		} else
+		{
+			if (stack == NULL)
+			{
+				printf("Error, stack should not be NULL\n");
+			}
+			if ((stack -> head -> next) == NULL)
+			{
+				done = 1;
+			} else
+			{
+				Stack_popPopCombinePush(stack);
+			}
+		}
+	}
+
+	HuffNode * final = stack -> head -> tree;
+
+	free(stack -> head);
+
+	free(stack);
+
+	return (final);
 }
